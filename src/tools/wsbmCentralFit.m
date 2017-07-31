794c72edb7187113626a47eca75acbd49f251e74
faskowit <jfaskowi@iu.edu>
2017-07-31 17:49:01 -0400

    added functioality for numTrials pattern

--- src/tools/wsbmCentralFit.m
+++ src/tools/wsbmCentralFit.m
@@ -1,4 +1,4 @@
-function [ centralModel , allModels ] = wsbmCentralFit( adjMat, rStruct , modelInputs , numFits , priorMu)
+function [ centralModel , allModels ] = wsbmCentralFit( adjMat, rStruct , modelInputs , numFits , priorMu, numTrialPttrn)
 % will fit wsbm specificed number iterations
 % and will return 'most central' based on variation of information between
 % community labels
@@ -6,6 +6,8 @@ function [ centralModel , allModels ] = wsbmCentralFit( adjMat, rStruct , modelI
 % user has the option to also provide prior labels to additionally multiply
 % by when computing the central mode. this will be useful if you want to
 % keep the central model 'near' a prior 
+
+% edit... numFits can be a vector, specifying num fits at each level of vec
 
 if nargin < 4
     disp('need more args')
@@ -17,14 +19,38 @@ if nargin < 5
     priorMu = '';
 end
 
+if nargin < 6
+    numTrialPttrn = [];
+end
+
 tempModelStruct = struct() ; 
 
 %fit numFits times at each iter
 for idx=1:numFits % model fits per iteration 
-
-    [~,tempModel] = wsbm(adjMat, ...
-        rStruct, ...
-        modelInputs{:} ) ;
+    
+    if isempty(numTrialPttrn)
+        
+        [~,tempModel] = wsbm(adjMat, ...
+            rStruct, ...
+            modelInputs{:} ) ;
+    else
+        
+       %begin with uninformative prior
+       mu_prior = make_WSBM_prior(...
+           ones(size(rStruct,1),size(adjMat,1)), 0) ;
+        
+       for jdx=1:numel(numTrialPttrn)
+        
+            [~,tempModel] = wsbm(adjMat, ...
+                rStruct, ...
+                modelInputs{:},...
+                'numTrials', numTrialPttrn(jdx), ...
+                'mu_0', mu_prior ) ;
+           
+            mu_prior = make_WSBM_prior(tempModel,2) ;
+       end
+           
+    end
 
     % to save space
     tempModel = rmfield(tempModel,'Data') ;
