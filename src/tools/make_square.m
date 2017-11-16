5f0961750a987e9bfca70f673693e979278fed72
faskowit <jfaskowi@iu.edu>
2017-11-16 14:47:03 -0500

    second output to make square

--- src/tools/make_square.m
+++ src/tools/make_square.m
@@ -1,7 +1,8 @@
-function [ a ] = make_square(M)
+function [ a , aFull ] = make_square(M)
 % make triu + diag outta vect
 
 b = length(M) ;
 s = sqrt(2 * b + 0.25) - 0.5 ;
 a = triu(ones(s),0) ;
 a(~~a) = M' ;
+aFull = a + triu(a,1)';
